0ba3adb7e6f56d8a1ce28ebe82ba162518801934
Jaime Aricapa <ja95aricapa@gmail.com>
2022-04-13 19:10:08 -0500

    update pycodestyle

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -2,10 +2,9 @@
 """
 0-pascal_triangle
 """
-from ast import Num
 
 
-def pascal_triangle(number: int):
+def pascal_triangle(number):
     """
     Create a list of numbers for the pascal triange
     """
@@ -13,7 +12,10 @@ def pascal_triangle(number: int):
     row = []
     prev_row = []
     for i in range(0, number + 1):
-        row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1] + prev_row[j] or 1 for j in range(0, i)]
+        row = [
+            j > 0 and j < i - 1 and i > 2 and
+            prev_row[j-1] + prev_row[j] or 1 for j in range(0, i)
+        ]
         prev_row = row
         triangle += [row]
     if number > 0:
