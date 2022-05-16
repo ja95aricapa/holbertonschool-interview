02f6a311cbd9a1916046f27b50477a1f3415bf57
Jaime <ja95aricapa@gmail.com>
2022-05-16 04:42:23 -0500

    upload files

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -3,16 +3,24 @@
 Main file for testing
 """
 
+# minOperations = __import__('0-minoperations').minOperations
+
+# n = 9
+# print("Min number of operations to reach {} characters: {}"
+#       .format(n, minOperations(n)))
+
+# n = 4
+# print("Min number of operations to reach {} characters: {}"
+#       .format(n, minOperations(n)))
+
+# n = 12
+# print("Min number of operations to reach {} characters: {}"
+#       .format(n, minOperations(n)))
+
+import sys
+
 minOperations = __import__('0-minoperations').minOperations
 
-n = 9
-print("Min number of operations to reach {} characters: {}"
-      .format(n, minOperations(n)))
-
-n = 4
-print("Min number of operations to reach {} characters: {}"
-      .format(n, minOperations(n)))
-
-n = 12
+n = 2147483640
 print("Min number of operations to reach {} characters: {}"
       .format(n, minOperations(n)))
