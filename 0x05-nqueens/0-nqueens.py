9c0e3588bc3b0e728c64ca5f72074868427e9226
Jaime Aricapa <ja95aricapa@gmail.com>
2022-09-01 05:51:57 -0500

    fix update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,129 +1,43 @@
 #!/usr/bin/python3
 """
-0-nqueens
+Module that solves the N Queens puzzle
 """
-
-import sys
-
-
-def validate(args):
-    """
-    Validate inputs
-
-    Args:
-        args ([type]): [description]
-    Returns:
-        [type]: [description]
-    """
-    if len(args) == 2:
-
-        try:
-            number = int(args[1])
-            if number < 4:
-                print("N must be at least 4")
-                exit(1)
-        except Exception:
-            print("N must be a number")
-            exit(1)
-
-        return number
-    else:
-        print("Usage: nqueens N")
-        exit(1)
-
-
-def solveNQ(number):
-    """
-    Solve N queens problem
-
-    Args:
-        number ([type]): [description]
-    Returns:
-        [type]: [description]
-    """
-    board = []
-    for i in range(number):
-        numbers = []
-        for j in range(number):
-            numbers.append(0)
-        board.append(numbers)
-
-    if not solveNQUtil(board, 0, number):
-        return False
-
-    return True
-
-
-def solveNQUtil(board, col, number):
-    """
-    Util function for the solveNQ function
-
-    Args:
-        board ([type]): [description]
-        number ([type]): [description]
-        col ([type]): [description]
-    Returns:
-        [type]: [description]
-    """
-    if (col == number):
-        print_board(board)
-        return True
-    answer = False
-
-    for i in range(number):
-        if (isSafe(board, i, col, number)):
-            board[i][col] = 1
-            answer = solveNQUtil(board, col + 1, number) or answer
-            board[i][col] = 0
-
-    return answer
-
-
-def isSafe(board, row, col, number):
-    """
-    check if a queen can be placed in a given row, col position
-
-    Args:
-        board ([type]): [description]
-        row ([type]): [description]
-        col ([type]): [description]
-        number ([type]): [description]
-    Returns:
-        [type]: [description]
-    """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, number, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def print_board(board):
-    """
-    print the board
-
-    Args:
-        board ([type]): [description]
-    """
-    new_board = []
-    for i, row in enumerate(board):
-        value = []
-        for j, col in enumerate(row):
-            if col == 1:
-                value.append(i)
-                value.append(j)
-        new_board.append(value)
-
-    print(new_board)
+from sys import argv, exit
 
 if __name__ == "__main__":
-    number = validate(sys.argv)
-    solveNQ(number)
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+
+    solution = []
+
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
+
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
+
+    solve_queens(0, n, solution)
