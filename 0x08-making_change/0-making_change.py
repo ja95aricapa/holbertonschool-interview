a01386b2e0e2924ccd0640b0c146f5be4900818a
Jaime Aricapa <ja95aricapa@gmail.com>
2022-09-21 09:05:29 -0500

    upload files

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-"""A Dynamic Programming based Python3 program to
-    find minimum of coins to make a given change total"""
-import sys
+'''Making Change module'''
 
 
 def makeChange(coins, total):
-
-    if (total <= 0):
+    '''Function that makes change for a given total'''
+    if total < 1:
         return 0
-
-    size = len(coins)
-
-    table = [0 for i in range(total + 1)]
-
-    table[0] = 0
-
-    for i in range(1, total + 1):
-        table[i] = sys.maxsize
-
-    for i in range(1, total + 1):
-
-        for j in range(size):
-            if (coins[j] <= i):
-                sub_res = table[i - coins[j]]
-                if (sub_res != sys.maxsize and
-                        sub_res + 1 < table[i]):
-                    table[i] = sub_res + 1
-
-    if table[total] == sys.maxsize:
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
         return -1
-
-    return table[total]
